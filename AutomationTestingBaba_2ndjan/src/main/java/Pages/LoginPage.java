72c213239e6e917cbcefb817b1edec17b860ab26
RahulSinha00009183 <rahul.kumarsinha900@gmail.com>
2024-06-27 14:32:59 +0530

    Automation

---
+++ AutomationTestingBaba_2ndjan/src/main/java/Pages/LoginPage.java
@@ -0,0 +1,43 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import baseLibrary.BaseLibrary;
+
+public class LoginPage extends BaseLibrary{
+
+	public LoginPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+     @FindBy(xpath = "//button[text()='×']")
+	private WebElement close;
+
+    @FindBy(xpath = "//a[text()='Practice']")
+    private WebElement Practice;
+     
+	public void clickonClose() {
+		close.click();
+	}
+
+	public void Practiceclick()
+	{
+		Practice.click();
+	}
+   public void geturl()
+   {
+	  String url= driver.getCurrentUrl();
+	  System.out.println(url);
+   }
+	public void gettitle()
+    {
+    	String val=driver.getTitle();
+    	System.out.println(val);
+    }
+	
+}
+
---
+++ AutomationTestingBaba_2ndjan/src/main/java/excelUtility/excelUtility.java
@@ -0,0 +1,6 @@
+package excelUtility;
+
+public interface excelUtility {
+
+	public String readDatafrmExcel(int Sheetno, int rno, int colno);
+}
